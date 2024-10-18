78ecac15006706b0eeab1087aede592d43abc993
Vinnnm <thanthtoo1285@gmail.com>
2024-10-18 22:58:39 +0630

    Add Navbar

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -5,6 +5,20 @@ const router = createBrowserRouter([
     {
       path: "/",
       element: <App/>,
+      children: [
+        {
+          path: "/",
+          element: <h1>Home</h1>
+        },
+        {
+          path: "/orders",
+          element: <div>Orders</div>
+        },
+        {
+          path: "/about",
+          element: <div>About</div>
+        }
+      ]
     },
 ]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FFCE1A',
+        'secondary': '#0D0842',
+        'blackBG': '#F3F3F3',
+        'favorite': '#FF5841'
+      },
+      fontFamily: {
+        'primary': ["Montserrat", "sans-serif"],
+        'secondary': ["Nunito Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
