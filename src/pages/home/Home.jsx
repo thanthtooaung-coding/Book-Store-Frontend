6e72b997d0e405cff56d663612c5a651e49f75f3
Vinnnm <thanthtoo1285@gmail.com>
2024-10-19 23:37:40 +0630

    Add Recommend

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommend from './Recommend'
 
 const Home = () => {
   return (
     <>
         <Banner />
         <TopSellers />
+        <Recommend />
     </>
   )
 }
